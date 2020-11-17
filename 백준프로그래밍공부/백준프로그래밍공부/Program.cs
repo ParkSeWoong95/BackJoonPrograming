811fc1e31603a993222248168e872c652aac670c
ParkSeWoong <atpdnd@gmail.com>
2020-11-17 20:57:56 +0900

    first commit

---
+++ 백준프로그래밍공부/백준프로그래밍공부/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace 백준프로그래밍공부
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] timeSplit = input.Split(' ');
+
+            int hour = int.Parse(timeSplit[0]);
+            int minute = int.Parse(timeSplit[1]);
+
+            minute = minute - 45;
+            if (minute > 0)
+            {
+                Console.WriteLine($"{hour} {minute}");
+            }
+            else if (minute < 0)
+            {
+                if (hour == 0)
+                {
+                    hour = 23;
+                }
+                else
+                {
+                    hour -= 1;
+                }
+                minute += 60;
+                Console.WriteLine($"{hour} {minute}");
+            }
+        }
+    }
+}
+
