064e4e2e140f65c2a35580ee06cd43157315da46
ParkSeWoong <atpdnd@gmail.com>
2020-11-19 00:58:27 +0900

    10171 - 고양이 그리기

---
+++ 10171 - 고양이 그리기/10171 - 고양이 그리기/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _10171___고양이_그리기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
