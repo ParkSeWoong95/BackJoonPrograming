95c4741ed7a78332c2f953fefc14a240f6543d09
ParkSeWoong <atpdnd@gmail.com>
2020-11-19 00:11:40 +0900

    Hello World 출력

---
+++ 2557 - Hello World/2557 - Hello World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2557___Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
