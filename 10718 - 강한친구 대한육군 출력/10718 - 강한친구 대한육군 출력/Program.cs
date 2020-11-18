8bfece89a740e2c5a78da1da08f296f2ac9b3a19
ParkSeWoong <atpdnd@gmail.com>
2020-11-19 00:27:02 +0900

    10718 - 강한친구 대한육군 출력 / c#기준 채점 오류발생

---
+++ 10718 - 강한친구 대한육군 출력/10718 - 강한친구 대한육군 출력/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace 백준프로그래밍공부
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("강한친구 대한육군");
+            Console.WriteLine("강한친구 대한육군");
+        }
+    }
+}
