2cb454eeda8126b8b773e65e499cf8ad3677a71a
ParkSeWoong <atpdnd@gmail.com>
2020-11-19 14:36:12 +0900

    1부터 n까지의 합을 구하는 문제, 반복문 없이도 가능

---
+++ 8393 - 합/8393 - 합/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _8393___합
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int A = 0;
+
+            for (int i = n; 0 <= i; i--)
+            {
+                A += i;
+            }
+            Console.WriteLine(A);
+        }
+    }
+}
