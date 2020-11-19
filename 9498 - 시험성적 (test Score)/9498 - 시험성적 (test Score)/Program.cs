54d043c504bab48fc14848b6f0ac84816452c671
ParkSeWoong <atpdnd@gmail.com>
2020-11-19 14:28:28 +0900

    시험 점수를 성적으로 바꾸는 문제

---
+++ 9498 - 시험성적 (test Score)/9498 - 시험성적 (test Score)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _9498___시험성적__test_Score_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 시험성적
+            int testScore = int.Parse(Console.ReadLine());
+
+            if (testScore >= 90 && testScore <= 100)
+            {
+                Console.WriteLine("A");
+            }
+            else if (testScore >= 80 && testScore < 90)
+            {
+                Console.WriteLine("B");
+            }
+            else if (testScore >= 70 && testScore < 80)
+            {
+                Console.WriteLine("C");
+            }
+            else if (testScore >= 60 && testScore < 70)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+        }
+    }
+}
