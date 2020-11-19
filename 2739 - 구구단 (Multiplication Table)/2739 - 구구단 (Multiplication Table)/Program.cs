9d21f29f1bbf763df0ec967d34339087e468772d
ParkSeWoong <atpdnd@gmail.com>
2020-11-19 14:32:36 +0900

    구구단 출력하는 문제

---
+++ 2739 - 구구단 (Multiplication Table)/2739 - 구구단 (Multiplication Table)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2739___구구단__Multiplication_Table_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            if (N >= 1 && N < 10)
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    Console.WriteLine($"{N} * {i} = {N * i}");
+                }
+            }
+        }
+    }
+}
