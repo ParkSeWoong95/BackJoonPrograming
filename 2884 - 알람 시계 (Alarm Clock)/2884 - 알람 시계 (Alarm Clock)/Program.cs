8fe2740c1d02c496933be5ea819422bca45f875d
ParkSeWoong <atpdnd@gmail.com>
2020-11-19 14:29:27 +0900

    시간 뺄셈 문제

---
+++ 2884 - 알람 시계 (Alarm Clock)/2884 - 알람 시계 (Alarm Clock)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _2884___알람_시계__Alarm_Clock_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split(' ');
+            int h = int.Parse(a[0]);
+            int m = int.Parse(a[1]);
+            if (h < 0 || h > 24 || m < 0 || m > 59) return;
+
+            if (m < 45)
+            {
+                h--;
+                m += 15;
+
+                if (h < 0)
+                {
+                    h = 23;
+                }
+            }
+            else
+            {
+                m -= 45;
+            }
+
+            Console.WriteLine("{0} {1}", h, m);
+        }
+    }
+}
