d179d2c395aa9bf8e60a6bc90099eb9489149f5e
ParkSeWoong <atpdnd@gmail.com>
2020-11-19 14:35:32 +0900

    A+B를 여러번 출력하는 문제

---
+++ 10950 - A+B 를 여러번 출력/10950 - A+B 를 여러번 출력/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10950___A_B_를_여러번_출력
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            int[] A = new int[T];
+            int[] B = new int[T];
+
+            for (int i = 0; i < T; i++)
+            {
+                string[] number = Console.ReadLine().Split(' ');
+                A[i] = int.Parse(number[0]);
+                B[i] = int.Parse(number[1]);
+            }
+            for (int j = 0; j < T; j++)
+            {
+                Console.WriteLine("{0}", A[j] + B[j]);
+            }
+        }
+    }
+}
