46a591bad3ceaa920759896e67163a49ecf6ec2a
ParkSeWoong <atpdnd@gmail.com>
2020-11-19 14:27:50 +0900

    두 수를 비교한 결과를 출력하는 문제

---
+++ 1330 - 두 수 비교하기 (compare two number)/1330 - 두 수 비교하기 (compare two number)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1330___두_수_비교하기__compare_two_number_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] number = Console.ReadLine().Split();
+            int A = int.Parse(number[0]);
+            int B = int.Parse(number[1]);
+
+            if (A > B)
+            {
+                Console.WriteLine(">");
+            }
+            else if (A < B)
+            {
+                Console.WriteLine("<");
+            }
+            else if (A == B)
+            {
+                Console.WriteLine("==");
+            }
+        }
+    }
+}
