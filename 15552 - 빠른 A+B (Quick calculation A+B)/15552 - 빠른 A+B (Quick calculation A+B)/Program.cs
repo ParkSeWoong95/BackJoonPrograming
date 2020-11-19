c2e0e8d4c6a8ea315e42424e1475d5c9397d0cfe
ParkSeWoong <atpdnd@gmail.com>
2020-11-19 14:37:59 +0900

    빠르게 입력받고 출력하는 문제

---
+++ 15552 - 빠른 A+B (Quick calculation A+B)/15552 - 빠른 A+B (Quick calculation A+B)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace _15552___빠른_A_B__Quick_calculation_A_B_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            int[] a = new int[T];
+            int[] b = new int[T];
+
+            for (int i = 0; i < T; i++)
+            {
+                string[] number = Console.ReadLine().Split(' ');
+                a[i] = int.Parse(number[0]);
+                b[i] = int.Parse(number[1]);
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int j = 0; j < T; j++)
+            {
+                stringBuilder.AppendLine($"{a[j] + b[j]}");
+            }
+            Console.WriteLine(stringBuilder.ToString()?.Trim());
+
+            Console.ReadKey();
+        }
+    }
+}
