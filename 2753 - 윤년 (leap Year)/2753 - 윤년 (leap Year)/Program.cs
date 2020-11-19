8290babf60f4af421f67c8d6948436d1f6b70d15
ParkSeWoong <atpdnd@gmail.com>
2020-11-19 14:28:49 +0900

    윤년을 판별하는 문제

---
+++ 2753 - 윤년 (leap Year)/2753 - 윤년 (leap Year)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2753___윤년__leap_Year_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int leapYear = int.Parse(Console.ReadLine());
+
+            if (leapYear % 4 == 0 && leapYear % 100 != 0 || leapYear % 400 == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
