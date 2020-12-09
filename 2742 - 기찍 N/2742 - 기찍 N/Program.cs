0cb671a41eca21998d10596af1f91fa7d3d1ae1c
ParkSeWoong95 <atpdnd@gmail.com>
2020-12-09 21:32:28 +0900

    문자열 포맷팅 생각하기

---
+++ 2742 - 기찍 N/2742 - 기찍 N/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace _2742___기찍_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder stringbuilder = new StringBuilder();
+
+            for (int i = n; i > 0; i--)
+            {
+                stringbuilder.AppendLine($"{i}");
+            }
+            Console.WriteLine(stringbuilder.ToString());
+        }
+    }
+}
