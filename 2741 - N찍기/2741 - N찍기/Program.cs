6f42b18c7610bd3a6bf8238f5fe3fa509af2d42f
ParkSeWoong95 <atpdnd@gmail.com>
2020-12-09 21:31:54 +0900

    N찍기 (stringbuilder 사용)

---
+++ 2741 - N찍기/2741 - N찍기/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace _2741___N찍기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            StringBuilder stringbuilder = new StringBuilder();
+
+            for (int i = 1; i <= N; i++)
+            {
+                stringbuilder.AppendLine($"{i}");
+            }
+
+            Console.WriteLine(stringbuilder.ToString());
+        }
+    }
+}
