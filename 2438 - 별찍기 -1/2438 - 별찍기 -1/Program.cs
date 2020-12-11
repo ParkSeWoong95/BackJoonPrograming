c225749d60bcc9ba96cc64f46990f445612fc9bf
ParkSeWoong95 <atpdnd@gmail.com>
2020-12-11 11:49:53 +0900

    별찍기 1

---
+++ 2438 - 별찍기 -1/2438 - 별찍기 -1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2438___별찍기__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
