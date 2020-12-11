161b34c325b172e004385dd32704486feb09dbc6
ParkSeWoong95 <atpdnd@gmail.com>
2020-12-11 11:48:56 +0900

    x보다 작은 수

---
+++ 10871 - X보다 작은 수/10871 - X보다 작은 수/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+namespace _10871___X보다_작은_수
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputInteger = Console.ReadLine().Split();
+            int n = int.Parse(inputInteger[0]);
+            int x = int.Parse(inputInteger[1]);
+            int[] array = new int[n];
+
+            StringBuilder build = new StringBuilder();
+            string[] stringNumber = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(stringNumber[i]);
+                if (array[i] < x)
+                {
+                    build.Append(array[i] + " ");
+                }
+            }
+            Console.WriteLine(build.ToString());
+        }
+    }
+}
