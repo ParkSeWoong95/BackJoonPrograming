5dcfa3a23e8983a7f2bf525249b02095e00f0b7c
ParkSeWoong95 <atpdnd@gmail.com>
2020-12-11 11:51:56 +0900

    .

---
+++ 10951 - A+B -4/10951 - A+B -4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+namespace _10951___A_B__4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string number = Console.ReadLine();
+                if (number == null)
+                {
+                    break;
+                }
+                string[] input = number.Split();
+                int A = int.Parse(input[0]);
+                int B = int.Parse(input[1]);
+                Console.WriteLine(A + B);
+            }
+        }
+    }
+}
---
+++ 10952 - A+B -5/10952 - A+B -5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+namespace _10952___A_B__5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            while (true)
+            {
+                string[] inputNumber = Console.ReadLine().Split();
+                int A = int.Parse(inputNumber[0]);
+                int B = int.Parse(inputNumber[1]);
+                int sum = A + B;
+
+                if (sum == 0)
+                {
+                    break;
+                }
+                sb.Append(sum + "\n");
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
